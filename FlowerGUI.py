8e218db72fbcfefab4888ceb5933276ad7202cdf
ImTani <tanishkdevil@gmail.com>
2022-08-25 01:35:41 +0530

    Better GUI, minor testing.

--- FlowerGUI.py
+++ FlowerGUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import filedialog
 import customtkinter as ctk
 from PIL import Image, ImageTk
 import sys
@@ -15,13 +16,20 @@ class NewFrame:
         root = ctk.CTk()
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("blue")
-        root.title("FlowerGUI")
+        root.title("PushpSoochak")
         root.geometry('1000x700')
         frame = ctk.CTkFrame(root)
         frame.pack(padx=50, pady=70, fill="both", expand=True)
         label = ctk.CTkLabel(frame, text=Label1_Text, text_font=FontStyle)
         label.pack(padx=10, pady=12)
-        FLimage = ImageTk.PhotoImage(Image.open(ImagePath))
+        myImage = Image.open(ImagePath)
+        myImage1 = myImage.resize((400, 400), Image.ANTIALIAS)
+        FLimage = ImageTk.PhotoImage(image=myImage1)
+        # old_height = FLimage.height()
+        # old_width = FLimage.width()
+        # scale_w = 300 / old_width
+        # scale_h = 300 / old_height
+        # FLimage._PhotoImage__photo.zoom(scale_w, scale_h)
         imageLabel = ctk.CTkLabel(master=frame, image=FLimage)
         imageLabel.pack(pady=30, padx=10)
         # label2 = ctk.CTkLabel(frame, text=BelowImage_Text)
--- Recogniser.py
+++ Recogniser.py
@@ -3,11 +3,17 @@ import tensorflow as tf
 from tensorflow import keras
 from keras_preprocessing import image
 import FlowerGUI as fl
+from tkinter import filedialog
 
-class_names = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+class_names = ['Daisy', 'Dandelion', 'Rose', 'Sunflower', 'Tulip']
 
-model = tf.keras.models.load_model("modelTest")
-path = "Predictions/surajmukhi.jpg"
+# model = tf.keras.models.load_model("modelTest")
+
+TF_MODEL_FILE_PATH = 'model.tflite'  # The default path to the saved TensorFlow Lite model
+
+interpreter = tf.lite.Interpreter(model_path=TF_MODEL_FILE_PATH)
+
+path = filedialog.askopenfilename(title="Choose An Image", filetypes=[('image files', ('.png', '.jpg'))])
 
 img = tf.keras.utils.load_img(
     path, target_size=(180, 180)
@@ -16,10 +22,23 @@ img = tf.keras.utils.load_img(
 img_array = tf.keras.utils.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0)  # Create a batch
 
-result = model.predict(img_array)
-score = tf.nn.softmax(result[0])
+# result = model.predict(img_array)
+# score = tf.nn.softmax(result[0])
+
+classify_lite = interpreter.get_signature_runner('serving_default')
+classify_lite
+
+predictions_lite = classify_lite(sequential_5_input=img_array)['outputs']
+score_lite = tf.nn.softmax(predictions_lite)
+
+# assert np.allclose(result, predictions_lite)
 
 fl.NewFrame.MakeTrivia(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score)),
+    "The flower in this image is most likely a {} with a {:.2f} % confidence."
+    .format(class_names[np.argmax(score_lite)], 100 * np.max(score_lite)),
     ImagePath=path)
+
+# fl.NewFrame.MakeTrivia(
+#     "This image most likely belongs to {} with a {:.2f} percent confidence."
+#     .format(class_names[np.argmax(score)], 100 * np.max(score)),
+#     ImagePath=path)
