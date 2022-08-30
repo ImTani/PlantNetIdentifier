b85b751b660ed5f6fcab0d4426189aea730adaec
ImTani <tanishkdevil@gmail.com>
2022-08-30 17:11:26 +0530

    Cleaning the repo

--- installer.bat
+++
@@ -1,6 +0,0 @@
-pyinstaller --noconfirm --log-level=WARN ^
-    --onedir --windowed ^
-    --add-data="E:/My Programs/Python/PushpSoochak/PSVenv/Lib/site-packages/customtkinter;customtkinter/" ^
-    --upx-dir "upx.exe" ^
-    --distpath ""./pyInstaller/dist" ^
-    Recogniser.spec
